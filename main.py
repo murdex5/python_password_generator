384ae645ba3e5a4b98f51380b448491ea3f6abe2
murdex5 <fkadupiige@gmail.com>
2023-03-11 23:18:50 +0100

    created a for loop that combines the special chars and the name ares to a list

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import random
+
+
 name = input("Enter your name here: ")
+
+#Add space to the string
 name_space = ''
 for ch in name:
    name_space = name_space + ch + ' '
-# print(result[:-1])
 
 name_arr = name_space.split()
-# print(name_arr)
 
-special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', "(", ")", '{', '}', '[', ']', ':', ';', '<', '>', ',', '.', '?', '/']
+special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', "(", ")", '{', '}', ':', ';', '<', '>', ',', '.', '?', '/', '1', '2', '3', '4', '5', '6']
 
-password_generated = ""
+password_generated = []
+
+#Adds the special characters and the name characters to the password_generated variable
+for i in name_arr:
+    password_generated.append(special_chars[random.randint(0, len(special_chars)-1)])
+    password_generated.insert(random.randint(0, len(name_arr)-1), i)
+
+#Convert the password_generated list to a string
+password = ''.join(password_generated)
+print(password)
