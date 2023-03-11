87042d2fb0bb159b38f21ff5f49144950720fb56
murdex5 <fkadupiige@gmail.com>
2023-03-11 22:58:23 +0100

    added speacial character array

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 name = input("Enter your name here: ")
 name_space = ''
 for ch in name:
-   result = name_space + ch + ' '
+   name_space = name_space + ch + ' '
 # print(result[:-1])
 
 name_arr = name_space.split()
 # print(name_arr)
+
+special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', "(", ")", '{', '}', '[', ']', ':', ';', '<', '>', ',', '.', '?', '/']
+
+password_generated = ""
