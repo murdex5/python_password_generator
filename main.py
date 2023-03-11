ab5713b2ff8c021ef582299bf43e0205f0515c23
murdex5 <fkadupiige@gmail.com>
2023-03-11 23:28:10 +0100

    Created a while loop to allow user to have various iterations of passwords

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import random
 
-
 name = input("Enter your name here: ")
+how_many = input("How many iterations do you need: ")
 
-#Add space to the string
-name_space = ''
-for ch in name:
-   name_space = name_space + ch + ' '
+def password_gen(names):
+    #Add space to the string
+    name_space = ''
+    for ch in name:
+        name_space = name_space + ch + ' '
 
-name_arr = name_space.split()
+    name_arr = name_space.split()
 
-special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', "(", ")", '{', '}', ':', ';', '<', '>', ',', '.', '?', '/', '1', '2', '3', '4', '5', '6']
+    special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', "(", ")", '{', '}', ':', ';', '<', '>', ',', '.', '?', '/', '1', '2', '3', '4', '5', '6']
 
-password_generated = []
+    password_generated = []
 
-#Adds the special characters and the name characters to the password_generated variable
-for i in name_arr:
-    password_generated.append(special_chars[random.randint(0, len(special_chars)-1)])
-    password_generated.insert(random.randint(0, len(name_arr)-1), i)
+    #Adds the special characters and the name characters to the password_generated variable
+    for i in name_arr:
+        password_generated.append(special_chars[random.randint(0, len(special_chars)-1)])
+        password_generated.insert(random.randint(0, len(name_arr)-1), i)
 
-#Convert the password_generated list to a string
-password = ''.join(password_generated)
-print(password)
+    #Convert the password_generated list to a string
+    password = ''.join(password_generated)
+    return password
+
+count_int = int(how_many)
+count = 0
+
+while count< count_int:
+    print(password_gen(name))
+    count += 1
